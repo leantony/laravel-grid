a4975598c24abb2fb48ffb5839460260c7d2618a
leantony <chachaantony@gmail.com>
2018-05-04 17:22:00 +0300

    make ajax modal work on bootstrap 4

--- src/resources/assets/js/grid.js
+++ src/resources/assets/js/grid.js
@@ -323,209 +323,14 @@ var _grids = _grids || {};
     _grids.modal = {};
 
     (function ($) {
-        'use strict';
-        var modal = function (options) {
-            var defaultOptions = {
-                // id of modal form template on page
-                modal_id: 'bootstrap_modal',
-                // id of notification element where messages will be displayed on the modal. E.g validation errors
-                notification_id: 'modal-notification',
-                // the id of the form that contains the data that will be sent to the server
-                form_id: 'modal_form',
-                // the class of the element that will trigger the modal. typically a link
-                modalTriggerSelector: '.show_modal_form',
-                // when the modal is shown
-                onShown: function (e, modal) {
-                    if (modal) {
-                        if (modal.options.onShown) {
-                            modal.options.onShown(e);
-                        }
-                    }
-                },
-                onHidden: function (e, modal) {
-                    $(this).removeData('bs.modal');
-                },
-                onShow: function (e, modal) {
-                    // display a loader, when the modal is being displayed
-                    var spinnerContent = '<div class="row"><div class="col-md-12"><div class="text-center"><i class="fa fa-spinner fa-spin fa-3x"></i></div></div></div>';
-                    $('#' + modal.options.modal_id).find('.modal-content').html(spinnerContent);
-                },
-                onLoaded: function (e, modal) {
-                    if (modal && modal.options) {
-                        modal.options.onLoaded(e);
-                    }
-                }
-            };
-            this.options = $.extend({}, defaultOptions, options || {});
-        };
 
-        /**
-         * show the modal
-         */
-        modal.prototype.show = function () {
-            var $this = this;
-            var modal_id = $this.options.modal_id;
-            var clickHandler = function (e) {
-                var modal_size = $(e).data('modal-size');
-                var modal = $('#' + modal_id);
-                if (!modal_size) {
-                    modal.find('.modal-dialog').addClass(modal_size);
-                }
-                var url = $(e).attr('href') || $(e).data('url');
-                modal
-                    .on('shown.bs.modal', function () {
-                        $this.options.onShown.call(this, e, $this);
-                    })
-                    .on('hidden.bs.modal', function () {
-                        $this.options.onHidden.call(this, e, $this);
-                    })
-                    .on('show.bs.modal', function () {
-                        $this.options.onShow.call(this, e, $this);
-                    })
-                    .on('loaded.bs.modal', function () {
-                        $this.options.onLoaded.call(this, e, $this);
-                    })
-                    .modal({
-                        remote: url,
-                        backdrop: 'static',
-                        refresh: true
-                    });
-            };
-
-            $(document.body).off('click.bs.modal').on('click.bs.modal', $this.options.modalTriggerSelector, function (e) {
-                e.preventDefault();
-                clickHandler(this);
+        $('.show_modal_form').on('click', function (e) {
+            e.preventDefault();
+            $('.modal-content').load($(this).attr("href"), function () {
+                $('#bootstrap_modal').modal({show: true});
             });
-        };
+        });
 
-        /**
-         * Render a bootstrap alert to the user. Requires the html to be inserted to the target element
-         * @param type
-         * @param response
-         * @returns {string}
-         */
-        modal.prototype.renderAlert = function (type, response) {
-            var validTypes = ['success', 'error', 'notice'], html = '';
-            if (typeof type === 'undefined' || ($.inArray(type, validTypes) < 0)) {
-                type = validTypes[0];
-            }
-            if (type === 'success') {
-                html += '<div class="alert alert-success">';
-            }
-            else if (type === 'error') {
-                html += '<div class="alert alert-danger">';
-            } else {
-                html += '<div class="alert alert-warning">';
-            }
-            html += '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>';
-            // add a heading
-            if (type === 'error') {
-                html += response.message || 'Please fix the following errors';
-                html = "<strong>" + html + "</strong>";
-                var errs = this.getLaravelValidationErrors(response.errors || {});
-                return html + errs + '</div>';
-            } else {
-                return html + response + '</div>'
-            }
-        };
-
-        /**
-         * Laravel returns validation error messages as a json object
-         * We process that to respective html here
-         * @param response
-         * @returns {string}
-         */
-        modal.prototype.getLaravelValidationErrors = function (response) {
-            var errorsHtml = '';
-            $.each(response, function (key, value) {
-                errorsHtml += '<li>' + value + '</li>';
-            });
-            return errorsHtml;
-        };
-
-        /**
-         * submit the modal form
-         */
-        modal.prototype.submitForm = function () {
-            var $this = this;
-
-            var submit_form = function (e) {
-                var form = $('#' + $this.options.form_id);
-                var data = form.serialize();
-                var action = form.attr('action');
-                var method = form.attr('method') || 'POST';
-                var originalButtonHtml = $(e).html();
-                var pjaxTarget = form.data('pjax-target');
-                $.ajax({
-                    type: method,
-                    url: action,
-                    data: data,
-                    dataType: 'json',
-                    success: function (response) {
-                        if (response.success) {
-                            var message = '<i class=\"fa fa-check\"></i> ';
-                            message += response.message;
-                            $('#' + $this.options.notification_id).html($this.renderAlert('success', message));
-                            // if a redirect is required...
-                            if (response.redirectTo) {
-                                setTimeout(function () {
-                                    window.location = response.redirectTo;
-                                }, response.redirectTimeout || 1000);
-                            } else {
-                                // hide the modal after 1000 ms
-                                setTimeout(function () {
-                                    $('#' + $this.options.modal_id).modal('hide');
-                                    if (pjaxTarget) {
-                                        // reload a pjax container
-                                        $.pjax.reload({container: pjaxTarget})
-                                    }
-                                }, 1000);
-                            }
-                        }
-                        else {
-                            // display message and hide modal
-                            var el = $('#' + $this.options.notification_id);
-                            el.html($this.renderAlert('error', response.message));
-                            setTimeout(function () {
-                                $('#' + $this.options.modal_id).modal('hide');
-                            }, 1000);
-                        }
-                    },
-                    beforeSend: function () {
-                        $(e).attr('disabled', 'disabled').html('Please wait....');
-                    },
-                    complete: function () {
-                        $(e).html(originalButtonHtml).removeAttr('disabled');
-                    },
-                    error: function (data) {
-                        var msg;
-                        // error handling
-                        switch (data.status) {
-                            case 500:
-                                msg = 'A server error occurred...';
-                                break;
-                            default:
-                                msg = $this.renderAlert('error', data.responseJSON);
-                                break;
-                        }
-                        // display errors
-                        var el = $('#' + $this.options.notification_id);
-                        el.html(msg);
-                    }
-                });
-            };
-
-            $('#' + $this.options.modal_id).off("click.bs.modal").on("click.bs.modal", '#' + $this.options.form_id + ' button[type="submit"]', function (e) {
-                e.preventDefault();
-                submit_form(this);
-            });
-        };
-
-        _grids.modal.init = function (options) {
-            var obj = new modal(options);
-            obj.show();
-            obj.submitForm();
-        };
     }(jQuery));
 
     /**
@@ -535,7 +340,7 @@ var _grids = _grids || {};
         // tooltip
         $('[data-toggle="tooltip"]').tooltip();
         // date picker
-        if(typeof daterangepicker !== 'function') {
+        if (typeof daterangepicker !== 'function') {
             console.warn('date picker option requires https://github.com/dangrossman/bootstrap-daterangepicker.git')
         } else {
             $('.grid-datepicker').daterangepicker({
