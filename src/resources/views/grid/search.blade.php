e80c066afd269f5c200908b1006cd6c2cd18cec4
leantony <chachaantony@gmail.com>
2018-05-05 00:55:43 +0300

    update input group refs to match bootstrap 4

--- src/resources/views/grid/search.blade.php
+++ src/resources/views/grid/search.blade.php
@@ -4,15 +4,11 @@
           data-{{ $k }}={{ $v }}
             @endforeach
     >
-        <div class="row">
-            <div class="input-group col-md-12">
-                <input type="text" class="form-control" name="{{ $name }}"
-                       placeholder="{{ $placeholder }}" value="{{ request($name) }}" required="required"/>
-                <span class="input-group-btn">
-                        <button class="btn btn-secondary" type="submit">
-                            <i class="fa fa-search"></i>
-                        </button>
-                    </span>
+        <div class="input-group mb-12">
+            <input type="text" class="form-control" name="{{ $name }}"
+                   placeholder="{{ $placeholder }}" value="{{ request($name) }}" required="required" aria-label="search">
+            <div class="input-group-append">
+                <button class="btn btn-outline-primary" type="submit"><i class="fa fa-search"></i></button>
             </div>
         </div>
     </form>
